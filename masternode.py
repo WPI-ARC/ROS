7c463a5e0682c777d66acd94ac8f6a77c97903b3
Riley Larkins <rdlarkins@wpi.edu>
2014-12-12 12:01:38 -0500

    Added messages for finger and hand state.

--- masternode.py
+++ masternode.py
@@ -10,3 +10,12 @@ class SoftHandMaster:
 	def _init_params(self):
 
 	def _init_pubsub(self):
+		# pub readings
+		#
+
+
+	# Send readings to linear fit
+	# Get linear fits
+	# Send data to grasp evaluator
+	# Command finger state
+	# Recieve finger state
